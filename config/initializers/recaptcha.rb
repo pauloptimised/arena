932880272bcae58b1d9d1626966eaad51cfc1109
James Doyley <jdoyley@gmail.com>
2015-09-21 15:37:32 +0100

    fix recaptcha

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+require "recaptcha/rails"
+
+Recaptcha.configure do |config|
+  config.public_key  = '6LdIdr0SAAAAAMAr5Y1IyD2q_W3D3RxJrQD9reng'
+  config.private_key = '6LdIdr0SAAAAAIphlh1U3FwDj-wPJyX8Bwuaa5qj'
+end
